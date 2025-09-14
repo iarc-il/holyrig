3786aa4b7d2a0612f75d1597d1b396c7b297b32b
Omer Sarig <omersarig@hiveware.io>
2025-09-14 13:37:01 +0300

    Remove omnirig translator

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ pub mod commands;
 pub mod data_format;
 pub mod gui;
 pub mod interpreter;
-pub mod omnirig_parser;
 pub mod parser;
 pub mod parser_errors;
 pub mod rig;
@@ -12,15 +11,12 @@ pub mod schema;
 pub mod schema_parser;
 pub mod semantic_analyzer;
 pub mod serial;
-pub mod translator;
 pub mod udp_server;
 pub mod wrapper;
 
 pub use interpreter::{Env, Interpreter, Value};
-pub use omnirig_parser::parse_ini_file;
 pub use schema_parser::{SchemaFile, parse_schema};
 pub use semantic_analyzer::{
     SemanticAnalyzer, SemanticError, parse_and_validate_with_schema,
     semantic_errors_to_parse_errors,
 };
-pub use translator::translate_omnirig_to_rig;
