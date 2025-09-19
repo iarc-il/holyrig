d13e5e21051360a592dc600b71ab9ee05a93b1b0
Omer Sarig <omersarig@hiveware.io>
2025-09-20 00:04:25 +0300

    Move rigctl client handling to a function

--- src/rigctld.rs
+++ src/rigctld.rs
@@ -2,9 +2,10 @@ use crate::Value;
 use anyhow::Result;
 use parking_lot::RwLock;
 use std::collections::HashMap;
+use std::net::SocketAddr;
 use std::sync::Arc;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::broadcast::Receiver;
 use tokio::sync::mpsc::Sender;
 
@@ -82,6 +83,149 @@ impl Default for DeviceStatus {
     }
 }
 
+async fn handle_client(
+    mut socket: TcpStream,
+    device_status: Arc<RwLock<DeviceStatus>>,
+    command_sender: Sender<ManagerCommand>,
+    addr: SocketAddr,
+) -> Result<()> {
+    let (reader, mut writer) = socket.split();
+
+    let mut reader = BufReader::new(reader);
+    let mut line = String::new();
+
+    loop {
+        line.clear();
+        if reader.read_line(&mut line).await.unwrap() == 0 {
+            break;
+        }
+
+        match parse_rigctl_command(&line) {
+            Ok(RigctlCommand::Quit) => break,
+            Ok(cmd) => {
+                let (command_name, params) = match cmd {
+                    RigctlCommand::SetFreq(freq) => ("set_freq", {
+                        let mut p = HashMap::new();
+                        p.insert("freq".to_string(), freq.to_string());
+                        p.insert("target".to_string(), "Current".to_string());
+                        p
+                    }),
+                    RigctlCommand::GetFreq => {
+                        let freq = {
+                            let device_status = device_status.read();
+                            if device_status.vfo == "A" {
+                                device_status.freq_a
+                            } else {
+                                device_status.freq_b
+                            }
+                        };
+                        writer
+                            .write_all(format!("{}\n", freq).as_bytes())
+                            .await
+                            .unwrap();
+                        continue;
+                    }
+                    RigctlCommand::SetMode(mode) => ("set_mode", {
+                        let mut p = HashMap::new();
+                        p.insert("mode".to_string(), mode);
+                        p
+                    }),
+                    RigctlCommand::GetMode => {
+                        let mode = { device_status.read().mode.clone() };
+                        writer
+                            .write_all(format!("{} 0\n", mode).as_bytes())
+                            .await
+                            .unwrap();
+                        continue;
+                    }
+                    RigctlCommand::SetVfo(vfo) => ("set_vfo", {
+                        let mut p = HashMap::new();
+                        p.insert("rx".to_string(), vfo.clone());
+                        p.insert("tx".to_string(), vfo);
+                        p
+                    }),
+                    RigctlCommand::GetVfo => {
+                        let vfo = { device_status.read().vfo.clone() };
+                        writer
+                            .write_all(format!("VFO{}\n", vfo).as_bytes())
+                            .await
+                            .unwrap();
+                        continue;
+                    }
+                    RigctlCommand::SetPtt(ptt) => (
+                        "transmit",
+                        HashMap::from([("tx".to_string(), ptt.to_string())]),
+                    ),
+                    RigctlCommand::GetPtt => {
+                        let transmit = { device_status.read().transmit };
+                        writer
+                            .write_all(format!("{}\n", transmit as i32).as_bytes())
+                            .await
+                            .unwrap();
+                        continue;
+                    }
+                    RigctlCommand::SetSplit(split) => ("set_split", {
+                        let mut p = HashMap::new();
+                        p.insert("split".to_string(), split.to_string());
+                        p
+                    }),
+                    RigctlCommand::GetSplit => {
+                        // Split status not available
+                        writer.write_all(b"0\n").await.unwrap();
+                        continue;
+                    }
+                    RigctlCommand::SetRit(_offset) => {
+                        // RIT offset not supported
+                        writer.write_all(b"RPRT -1\n").await.unwrap();
+                        continue;
+                    }
+                    RigctlCommand::GetRit => {
+                        let rit = { device_status.read().rit };
+                        writer
+                            .write_all(format!("{}\n", rit as i32).as_bytes())
+                            .await
+                            .unwrap();
+                        continue;
+                    }
+                    RigctlCommand::SetXit(_offset) => {
+                        // XIT offset not supported
+                        writer.write_all(b"RPRT -1\n").await.unwrap();
+                        continue;
+                    }
+                    RigctlCommand::GetXit => {
+                        let xit = { device_status.read().xit };
+                        writer
+                            .write_all(format!("{}\n", xit as i32).as_bytes())
+                            .await
+                            .unwrap();
+                        continue;
+                    }
+                    RigctlCommand::Quit => unreachable!(),
+                };
+
+                if let Err(e) = command_sender
+                    .send(ManagerCommand::ExecuteCommand {
+                        device_id: 0, // TODO: Support multiple devices
+                        command_name: command_name.to_string(),
+                        params,
+                    })
+                    .await
+                {
+                    eprintln!("Failed to send command: {}", e);
+                    break;
+                }
+
+                writer.write_all(b"RPRT 0\n").await.unwrap();
+            }
+            Err(e) => {
+                eprintln!("Error parsing command from {}: {}", addr, e);
+                writer.write_all(b"RPRT 1\n").await.unwrap();
+            }
+        }
+    }
+    Ok(())
+}
+
 pub async fn run_server(
     command_sender: Sender<ManagerCommand>,
     mut message_receiver: Receiver<ManagerMessage>,
@@ -94,133 +238,12 @@ pub async fn run_server(
     loop {
         tokio::select! {
             accept_result = listener.accept() => {
-                let (mut socket, addr) = accept_result?;
+                let (socket, addr) = accept_result?;
                 let command_sender = command_sender.clone();
 
                 let device_status = device_status.clone();
                 tokio::spawn(async move {
-                    let (reader, mut writer) = socket.split();
-                    let mut reader = BufReader::new(reader);
-                    let mut line = String::new();
-
-                    loop {
-                        line.clear();
-                        if reader.read_line(&mut line).await.unwrap() == 0 {
-                            break;
-                        }
-
-                        match parse_rigctl_command(&line) {
-                            Ok(RigctlCommand::Quit) => break,
-                            Ok(cmd) => {
-                                let (command_name, params) = match cmd {
-                                    RigctlCommand::SetFreq(freq) => {
-                                        ("set_freq", {
-                                            let mut p = HashMap::new();
-                                            p.insert("freq".to_string(), freq.to_string());
-                                            p.insert("target".to_string(), "Current".to_string());
-                                            p
-                                        })
-                                    },
-                                    RigctlCommand::GetFreq => {
-                                        let freq = {
-                                            let device_status = device_status.read();
-                                            if device_status.vfo == "A" {
-                                                device_status.freq_a
-                                            } else {
-                                                device_status.freq_b
-                                            }
-                                        };
-                                        writer.write_all(format!("{}\n", freq).as_bytes()).await.unwrap();
-                                        continue;
-                                    },
-                                    RigctlCommand::SetMode(mode) => {
-                                        ("set_mode", {
-                                            let mut p = HashMap::new();
-                                            p.insert("mode".to_string(), mode);
-                                            p
-                                        })
-                                    },
-                                    RigctlCommand::GetMode => {
-                                        let mode = { device_status.read().mode.clone() };
-                                        writer.write_all(format!("{} 0\n", mode).as_bytes()).await.unwrap();
-                                        continue;
-                                    },
-                                    RigctlCommand::SetVfo(vfo) => {
-                                        ("set_vfo", {
-                                            let mut p = HashMap::new();
-                                            p.insert("rx".to_string(), vfo.clone());
-                                            p.insert("tx".to_string(), vfo);
-                                            p
-                                        })
-                                    },
-                                    RigctlCommand::GetVfo => {
-                                        let vfo = { device_status.read().vfo.clone() };
-                                        writer.write_all(format!("VFO{}\n", vfo).as_bytes()).await.unwrap();
-                                        continue;
-                                    },
-                                    RigctlCommand::SetPtt(ptt) => {
-                                        ("transmit", HashMap::from([("tx".to_string(), ptt.to_string())]))
-                                    },
-                                    RigctlCommand::GetPtt => {
-                                        let transmit = { device_status.read().transmit };
-                                        writer.write_all(format!("{}\n", transmit as i32).as_bytes()).await.unwrap();
-                                        continue;
-                                    },
-                                    RigctlCommand::SetSplit(split) => {
-                                        ("set_split", {
-                                            let mut p = HashMap::new();
-                                            p.insert("split".to_string(), split.to_string());
-                                            p
-                                        })
-                                    },
-                                    RigctlCommand::GetSplit => {
-                                        // Split status not available
-                                        writer.write_all(b"0\n").await.unwrap();
-                                        continue;
-                                    },
-                                    RigctlCommand::SetRit(_offset) => {
-                                        // RIT offset not supported
-                                        writer.write_all(b"RPRT -1\n").await.unwrap();
-                                        continue;
-                                    },
-                                    RigctlCommand::GetRit => {
-                                        let rit = { device_status.read().rit };
-                                        writer.write_all(format!("{}\n", rit as i32).as_bytes()).await.unwrap();
-                                        continue;
-                                    },
-                                    RigctlCommand::SetXit(_offset) => {
-                                        // XIT offset not supported
-                                        writer.write_all(b"RPRT -1\n").await.unwrap();
-                                        continue;
-                                    },
-                                    RigctlCommand::GetXit => {
-                                        let xit = { device_status.read().xit };
-                                        writer.write_all(format!("{}\n", xit as i32).as_bytes()).await.unwrap();
-                                        continue;
-                                    },
-                                    RigctlCommand::Quit => unreachable!(),
-                                };
-
-                                if let Err(e) = command_sender
-                                    .send(ManagerCommand::ExecuteCommand {
-                                        device_id: 0, // TODO: Support multiple devices
-                                        command_name: command_name.to_string(),
-                                        params,
-                                    })
-                                    .await
-                                {
-                                    eprintln!("Failed to send command: {}", e);
-                                    break;
-                                }
-
-                                writer.write_all(b"RPRT 0\n").await.unwrap();
-                            },
-                            Err(e) => {
-                                eprintln!("Error parsing command from {}: {}", addr, e);
-                                writer.write_all(b"RPRT 1\n").await.unwrap();
-                            }
-                        }
-                    }
+                    handle_client(socket, device_status, command_sender, addr).await
                 });
             }
             Ok(msg) = message_receiver.recv() => {
