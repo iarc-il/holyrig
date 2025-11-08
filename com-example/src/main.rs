c3345052118d7f4dd7733bca59e60e63c9fd3448
Omer Sarig <omersarig@hiveware.io>
2025-11-08 23:03:06 +0200

    Start implementing working server

--- com-example/src/main.rs
+++ com-example/src/main.rs
@@ -1,12 +1,26 @@
-use windows::core::{implement, Result, GUID, PCWSTR};
+#![allow(non_camel_case_types)]
 
-use windows::Win32::Foundation::E_NOTIMPL;
+use std::ffi::c_void;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use windows::core::{implement, IUnknown, Result, GUID, PCWSTR};
+
+use windows::Win32::Foundation::{BOOL, CLASS_E_NOAGGREGATION, E_NOINTERFACE, E_NOTIMPL, HWND};
 use windows::Win32::System::Com::{
-    CoInitializeEx, CoUninitialize, IDispatch, IDispatch_Impl, ITypeInfo, COINIT_MULTITHREADED,
-    DISPATCH_FLAGS, DISPPARAMS, EXCEPINFO,
+    CoInitializeEx, CoRegisterClassObject, CoRevokeClassObject, CoUninitialize, IClassFactory,
+    IClassFactory_Impl, IDispatch, IDispatch_Impl, ITypeInfo, CLSCTX_LOCAL_SERVER,
+    COINIT_MULTITHREADED, DISPATCH_FLAGS, DISPPARAMS, EXCEPINFO, REGCLS_MULTIPLEUSE,
 };
 
 use windows::Win32::System::Variant::VARIANT;
+use windows::Win32::UI::WindowsAndMessaging::{
+    DispatchMessageW, GetMessageW, TranslateMessage, MSG,
+};
+
+const CLSID_SIMPLE_COM_OBJECT: GUID = GUID::from_u128(0x12345678_1234_1234_1234_123456789ABC);
+
+const IID_IUNKNOWN: GUID = GUID::from_u128(0x00000000_0000_0000_C000_000000000046);
+const IID_IDISPATCH: GUID = GUID::from_u128(0x00020400_0000_0000_C000_000000000046);
 
 #[implement(IDispatch)]
 struct SimpleComObject;
@@ -46,17 +60,95 @@ impl IDispatch_Impl for SimpleComObject {
     }
 }
 
+#[implement(IClassFactory)]
+struct SimpleComObjectFactory;
+
+impl IClassFactory_Impl for SimpleComObjectFactory {
+    fn CreateInstance(
+        &self,
+        punkouter: Option<&IUnknown>,
+        riid: *const GUID,
+        ppvobject: *mut *mut c_void,
+    ) -> Result<()> {
+        if punkouter.is_some() {
+            return Err(CLASS_E_NOAGGREGATION.into());
+        }
+
+        unsafe {
+            let requested_iid = *riid;
+
+            if requested_iid != IID_IUNKNOWN && requested_iid != IID_IDISPATCH {
+                *ppvobject = std::ptr::null_mut();
+                return Err(E_NOINTERFACE.into());
+            }
+
+            let instance: IDispatch = SimpleComObject.into();
+            *ppvobject = std::mem::transmute_copy(&instance);
+            std::mem::forget(instance);
+        }
+        Ok(())
+    }
+
+    fn LockServer(&self, _flock: BOOL) -> Result<()> {
+        Ok(())
+    }
+}
+
 fn main() -> Result<()> {
     unsafe {
         CoInitializeEx(None, COINIT_MULTITHREADED)?;
 
-        let com_object = SimpleComObject;
-        let _dispatch: IDispatch = com_object.into();
+        let factory: IClassFactory = SimpleComObjectFactory.into();
 
-        println!("COM IDispatch server initialized successfully!");
-        println!("SimpleComObject implements IDispatch with stub methods.");
+        let cookie = CoRegisterClassObject(
+            &CLSID_SIMPLE_COM_OBJECT,
+            &factory,
+            CLSCTX_LOCAL_SERVER,
+            REGCLS_MULTIPLEUSE,
+        )?;
 
+        println!("COM server started successfully!");
+        println!(
+            "CLSID: {{{:08X}-{:04X}-{:04X}-{:02X}{:02X}-{:02X}{:02X}{:02X}{:02X}{:02X}{:02X}}}",
+            CLSID_SIMPLE_COM_OBJECT.data1,
+            CLSID_SIMPLE_COM_OBJECT.data2,
+            CLSID_SIMPLE_COM_OBJECT.data3,
+            CLSID_SIMPLE_COM_OBJECT.data4[0],
+            CLSID_SIMPLE_COM_OBJECT.data4[1],
+            CLSID_SIMPLE_COM_OBJECT.data4[2],
+            CLSID_SIMPLE_COM_OBJECT.data4[3],
+            CLSID_SIMPLE_COM_OBJECT.data4[4],
+            CLSID_SIMPLE_COM_OBJECT.data4[5],
+            CLSID_SIMPLE_COM_OBJECT.data4[6],
+            CLSID_SIMPLE_COM_OBJECT.data4[7],
+        );
+        println!("Press Ctrl+C to stop the server...");
+
+        let running = Arc::new(AtomicBool::new(true));
+        let running_clone = running.clone();
+
+        ctrlc::set_handler(move || {
+            println!("\nReceived Ctrl+C, shutting down...");
+            running_clone.store(false, Ordering::SeqCst);
+        })
+        .expect("Error setting Ctrl-C handler");
+
+        let mut msg = MSG::default();
+        while running.load(Ordering::SeqCst) {
+            let result = GetMessageW(&mut msg, HWND::default(), 0, 0);
+            if result.0 == 0 || result.0 == -1 {
+                break;
+            }
+            TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+
+            std::thread::sleep(std::time::Duration::from_millis(100));
+        }
+
+        println!("Revoking class object...");
+        CoRevokeClassObject(cookie)?;
         CoUninitialize();
+        println!("Server stopped.");
     }
 
     Ok(())
