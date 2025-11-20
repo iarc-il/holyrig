201c6b735c04e16415da1f49a2c4cc61bb8f4aad
Omer Sarig <omersarig@hiveware.io>
2025-11-20 17:59:13 +0200

    Start working on the actual omnirig implementation

---
+++ omnirig/src/main.rs
@@ -0,0 +1,17 @@
+#![allow(non_camel_case_types)]
+
+use windows::Win32::System::Com::{CoInitializeEx, CoUninitialize, COINIT_MULTITHREADED};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    unsafe {
+        CoInitializeEx(None, COINIT_MULTITHREADED).ok()?;
+
+        println!("OmniRig COM server initialized");
+        println!("Press Ctrl+C to stop the server...");
+
+        CoUninitialize();
+        println!("Server stopped.");
+    }
+
+    Ok(())
+}
