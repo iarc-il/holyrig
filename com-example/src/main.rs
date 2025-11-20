22adadf7071d0dc31e56309ecc0b3dadd7c06c15
Omer Sarig <omersarig@hiveware.io>
2025-11-20 12:41:11 +0200

    Split the com example to files

--- com-example/src/main.rs
+++ com-example/src/main.rs
@@ -1,114 +1,24 @@
 #![allow(non_camel_case_types)]
 
-use std::cell::RefCell;
-use std::ffi::c_void;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
-use windows::core::{GUID, IUnknown, implement};
+use windows::core::GUID;
 
-use windows::Win32::Foundation::{CLASS_E_NOAGGREGATION, E_NOINTERFACE, HWND};
+use windows::Win32::Foundation::HWND;
 use windows::Win32::System::Com::{
     CLSCTX_LOCAL_SERVER, COINIT_MULTITHREADED, CoInitializeEx, CoRegisterClassObject,
-    CoRevokeClassObject, CoUninitialize, IClassFactory, IClassFactory_Impl, IDispatch,
-    IDispatch_Impl, REGCLS_MULTIPLEUSE,
+    CoRevokeClassObject, CoUninitialize, IClassFactory, REGCLS_MULTIPLEUSE,
 };
 
 use windows::Win32::UI::WindowsAndMessaging::{
     DispatchMessageW, GetMessageW, MSG, TranslateMessage,
 };
-use windows_core::{Interface, BOOL};
 
-use auto_dispatch::auto_dispatch;
+use crate::simple_object::SimpleObjectFactory;
+
+mod simple_object;
 
 const CLSID_SIMPLE_COM_OBJECT: GUID = GUID::from_u128(0x12345678_1234_1234_1234_123456789ABC);
-
-#[implement(IDispatch)]
-struct SimpleObject {
-    counter: RefCell<u32>,
-    flag: RefCell<bool>,
-}
-
-#[auto_dispatch]
-impl SimpleObject {
-    #[id(1)]
-    #[getter]
-    fn prop1(&self) -> Result<u32, HRESULT> {
-        Ok(*self.counter.borrow())
-    }
-
-    #[id(2)]
-    #[getter]
-    fn prop2(&self) -> Result<bool, HRESULT> {
-        Ok(*self.flag.borrow())
-    }
-
-    #[id(2)]
-    #[setter]
-    fn prop2(&self, value: bool) -> Result<(), HRESULT> {
-        *self.flag.borrow_mut() = value;
-        Ok(())
-    }
-
-    #[id(3)]
-    fn do_stuff(&self, value1: bool, value2: u16, value3: u16) -> Result<(), HRESULT> {
-        println!("Value1: {value1}");
-        println!("Value2: {value2}");
-        println!("Value3: {value3}");
-        if value1 {
-            println!("Result: {}", value2 * value3);
-        } else {
-            println!("Result: {}", value2 + value3);
-        }
-        Ok(())
-    }
-
-    #[id(4)]
-    fn do_other_stuff(&self, value1: bool, value3: i64) -> Result<u32, HRESULT> {
-        if value1 {
-            Ok((value3 * 3 + 1) as u32)
-        } else {
-            Ok((value3 / 2) as u32)
-        }
-    }
-}
-
-#[implement(IClassFactory)]
-struct SimpleObjectFactory;
-
-impl IClassFactory_Impl for SimpleObjectFactory_Impl {
-    fn CreateInstance(
-        &self,
-        punkouter: windows_core::Ref<IUnknown>,
-        riid: *const GUID,
-        ppvobject: *mut *mut c_void,
-    ) -> windows::core::Result<()> {
-        if punkouter.is_some() {
-            return Err(CLASS_E_NOAGGREGATION.into());
-        }
-
-        unsafe {
-            let requested_iid = *riid;
-
-            if requested_iid != IUnknown::IID && requested_iid != IDispatch::IID {
-                *ppvobject = std::ptr::null_mut();
-                return Err(E_NOINTERFACE.into());
-            }
-
-            let instance: IDispatch = SimpleObject {
-                counter: RefCell::new(0),
-                flag: RefCell::new(false),
-            }
-            .into();
-            *ppvobject = std::mem::transmute_copy(&instance);
-            std::mem::forget(instance);
-        }
-        Ok(())
-    }
-
-    fn LockServer(&self, _flock: BOOL) -> windows::core::Result<()> {
-        Ok(())
-    }
-}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     unsafe {
